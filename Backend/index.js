9d98853158812dc3d375fb62dad241a4444db288
PavanUmmagani07 <pavankalyanU@outlook.com>
2025-07-17 14:00:32 +0530

    deployment errors

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://syncroom-three.vercel.app",
     methods: ["GET", "POST"]
   }
 });
